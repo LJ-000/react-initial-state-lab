91b87f916c0130aa84ab80dcbd5763a72ba25b6e
Laura Nielsen <nielsen.j.laura@gmail.com>
2021-04-26 18:21:11 -0400

    commit tree fc646a03674898ad04ad4e129a1d98aac4e82784

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,31 @@
 // your Bomb code here!
+import React, {Component } from 'react';
+
+class Bomb extends Component {
+
+    constructor(props) {
+    super()
+
+    this.state = {
+    secondsLeft: props.initialCount 
+    }
+}
+
+    render() {
+        const message = this.state.secondsLeft === 0 ? 'Boom!' : `${this.state.secondsLeft} seconds left before I go boom!`;
+        
+        return (
+            <div>{message}</div>
+        )
+    }
+}
+
+export default Bomb; 
+
+
+// constructor(props) { //props that the component gets from its parent
+//     super()
+//     this.state = { //define initial state with a key of 'someKey' set to the 'someValue' prop
+//       someKey: props.someValue
+//     }
+//   }
